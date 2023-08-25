e8139b29510ef9bcc401ccc8cabff3bd4196d8c6
Peter <hoffmannpeter82@gmail.com>
2023-08-25 16:38:53 +0200

    Adjust styling for ParameterList

--- src/features/Dashboard.tsx
+++ src/features/Dashboard.tsx
@@ -15,8 +15,32 @@ export const Dashboard: React.FC = () => {
         unit: UnitType.AMOUNT,
         value: 0,
       },
+      {
+        id: "2",
+        title: t.parameters.weightOfDoughPiece,
+        unit: UnitType.GRAMS,
+        value: 0,
+      },
+      {
+        id: "3",
+        title: t.parameters.hydration,
+        unit: UnitType.PERCENT,
+        value: 0,
+      },
+      {
+        id: "4",
+        title: t.parameters.salt,
+        unit: UnitType.PERCENT,
+        value: 0,
+      },
+      {
+        id: "5",
+        title: t.parameters.amountPreDough,
+        unit: UnitType.PERCENT,
+        value: 0,
+      },
     ];
-  }, []);
+  }, [t.parameters.numberOfPizzas]);
 
   return (
     <>
--- src/features/parameterList/ParameterList.tsx
+++ src/features/parameterList/ParameterList.tsx
@@ -1,9 +1,10 @@
 import { Parameter } from "../parameter/Parameter";
 import { IParameterListProps } from "./IParameterListProps";
+import styles from "./ParameterList.module.css";
 
 export const ParameterList: React.FC<IParameterListProps> = (props) => {
   const items = props.parameters.map((parameter) => (
-    <div key={parameter.id}>
+    <div key={parameter.id} className={styles.parameterListItem}>
       <Parameter parameter={parameter} />
     </div>
   ));
