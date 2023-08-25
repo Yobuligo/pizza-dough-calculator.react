1e5a36e286ccbb4f65ea098e7543495757622cc7
Peter <hoffmannpeter82@gmail.com>
2023-08-25 15:13:13 +0200

    Implement useLanguage

--- src/hooks/useLanguage.ts
+++ src/hooks/useLanguage.ts
@@ -1,1 +1,7 @@
-export const useLanguage = ()=>{}
+import { LanguageType } from "../types/LanguageType";
+import { useLocalStorage } from "./useLocalStorage";
+
+export const useLanguage = () => {
+  const [language, setLanguage] = useLocalStorage("language", LanguageType.EN);
+  return [language, setLanguage];
+};
