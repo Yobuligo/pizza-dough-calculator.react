5f7ba3f139ad9163e1e4877a9ac54d9e8f6e8e1b
Peter <hoffmannpeter82@gmail.com>
2023-08-25 16:08:32 +0200

    Introduce useValue

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
+import { LabeledInput } from "./components/LabeledInput";
 import { AppContext } from "./context/AppContext";
 import { useTranslation } from "./hooks/useTranslation";
 
 const App: React.FC = () => {
+  const 
   const { t } = useTranslation();
 
-  return <AppContext.Provider value={{}}>{t.title}</AppContext.Provider>;
+  return <AppContext.Provider value={{}}>{t.title}
+  
+    <LabeledInput label={t.parameters.amountPreDough} />
+  </AppContext.Provider>;
 };
 
 export default App;
--- src/context/IAppContext.ts
+++ src/context/IAppContext.ts
@@ -1,1 +1,14 @@
-export interface IAppContext {}
+export interface IAppContext {
+    // numberOfPizzas: 
+    // "numberOfPizzas": "Number pizzas",
+    // "weightOfDoughPiece": "Weight of dough piece",
+    // "hydration": "Hydration",
+    // "salt": "Salt",
+    // "amountPreDough": "Amount pre dough",
+    // "risingTime": "Rising time",
+    // "short": "Short",
+    // "long": "Long",
+    // "yeastType": "Yeast type",
+    // "dryYeast": "Dry yeast",
+    // "freshYeast": "Fresh yeast"
+}
