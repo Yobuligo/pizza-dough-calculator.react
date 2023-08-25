07396ef1b9c11494e2ce479cb0ac6b9c39b26100
Peter <hoffmannpeter82@gmail.com>
2023-08-25 15:15:23 +0200

    Implement useTranslation

--- src/hooks/useTranslation.ts
+++ src/hooks/useTranslation.ts
@@ -1,5 +1,18 @@
 import * as translations from "../i18n";
+import { LanguageType } from "../types/LanguageType";
+import { useLanguage } from "./useLanguage";
 
 export const useTranslation = () => {
-  return { t: translations["en"] };
+  const [language] = useLanguage();
+  switch (language) {
+    case LanguageType.DE:
+      return { t: translations["de"] };
+
+    case LanguageType.EN:
+      return { t: translations["en"] };
+    default:
+      throw new Error(
+        `Error while getting translations. Language '${language}' is not supported.`
+      );
+  }
 };
