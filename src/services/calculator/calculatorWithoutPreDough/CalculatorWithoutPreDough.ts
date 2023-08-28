64199943545daa5e89a8b9f2950f477333b6ea70
Peter <hoffmannpeter82@gmail.com>
2023-08-28 14:26:41 +0200

    Implement CalculatorWithoutPreDough

--- src/services/calculator/calculatorWithoutPreDough/CalculatorWithoutPreDough.ts
+++ src/services/calculator/calculatorWithoutPreDough/CalculatorWithoutPreDough.ts
@@ -1,9 +1,19 @@
+import { TotalMassCalculator } from "../../totalMassCalculator/TotalMassCalculator";
 import { ICalculator } from "../core/ICalculator";
 import { IParameter } from "./IParameter";
 import { IResultWithoutPreDough } from "./IResultWithoutPreDough";
 
-export class CalculatorWithoutPreDough implements ICalculator<IParameter, IResultWithoutPreDough>{
-    calc(parameter: IParameter): IResultWithoutPreDough {
-        throw new Error("Method not implemented.");
-    }
+export class CalculatorWithoutPreDough
+  implements ICalculator<IParameter, IResultWithoutPreDough>
+{
+  calc(parameter: IParameter): IResultWithoutPreDough {
+    const totalMassResult = TotalMassCalculator.calc(parameter);
+    return {
+      flour: totalMassResult.totalFlour,
+      honey: totalMassResult.totalHoney,
+      salt: totalMassResult.totalSalt,
+      water: totalMassResult.totalWater,
+      yeast: totalMassResult.totalYeast,
+    };
+  }
 }
