19fc304490475d8d763136eebf30655ddcfb8de4
acoria <tewes.verena@gmail.com>
2023-08-28 15:28:17 +0200

    Set pre dough config on context

--- src/features/doughConfiguration/preDoughConfiguration/PreDoughConfiguration.tsx
+++ src/features/doughConfiguration/preDoughConfiguration/PreDoughConfiguration.tsx
@@ -1,12 +1,19 @@
-import { useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Switch } from "../../../components/switch/Switch";
 import { useTranslation } from "../../../hooks/useTranslation";
 import { ConfigurationItem } from "../components/configuration/ConfigurationItem";
 import styles from "./PreDoughConfiguration.module.css";
+import { AppContext } from "../../../context/AppContext";
 
 export const PreDoughConfiguration: React.FC = () => {
   const [preDoughUsed, setPreDoughUsed] = useState(false);
   const { t } = useTranslation();
+  const context = useContext(AppContext);
+
+  useEffect(() => {
+    context.usePreDough.setValue(preDoughUsed);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [preDoughUsed]);
 
   return (
     <ConfigurationItem configuration={{ name: t.parameters.preparePreDough }}>
@@ -15,7 +22,14 @@ export const PreDoughConfiguration: React.FC = () => {
           id="preDough"
           onChange={(_, checked) => setPreDoughUsed(checked)}
         />
-        <input id="preDoughPercentage" type="number" disabled={!preDoughUsed} />
+        <input
+          id="preDoughPercentage"
+          type="number"
+          disabled={!preDoughUsed}
+          onChange={(event) =>
+            context.amountPreDough.setValue(+event.target.value)
+          }
+        />
         <label className={styles.percentageLabel} htmlFor="preDoughPercentage">
           %
         </label>
