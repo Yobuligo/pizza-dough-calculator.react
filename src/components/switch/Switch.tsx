4ef613b77a58d8ba1e342050c1411d0916eb7ba5
acoria <tewes.verena@gmail.com>
2023-08-28 12:57:56 +0200

    Adjust switch color

--- src/components/switch/Switch.tsx
+++ src/components/switch/Switch.tsx
@@ -2,19 +2,25 @@ import MuiSwitch, { SwitchProps } from "@mui/material/Switch";
 import { alpha, styled } from "@mui/material/styles";
 
 export const Switch: React.FC<SwitchProps> = (props) => {
-  const colour = getComputedStyle(document.body).getPropertyValue(
-    "--secondaryColor"
-  );
+  const computedStyle = getComputedStyle(document.body);
+  const primaryColor = computedStyle.getPropertyValue("--primaryColor");
+  const secondaryColor = computedStyle.getPropertyValue("--secondaryColor");
 
   const ColouredSwitch = styled(MuiSwitch)(({ theme }) => ({
     "& .MuiSwitch-switchBase.Mui-checked": {
-      color: colour,
+      color: secondaryColor,
       "&:hover": {
-        backgroundColor: alpha(colour, theme.palette.action.hoverOpacity),
+        backgroundColor: alpha(
+          secondaryColor,
+          theme.palette.action.hoverOpacity
+        ),
       },
     },
+    "& .MuiSwitch-track": {
+      backgroundColor: primaryColor,
+    },
     "& .MuiSwitch-switchBase.Mui-checked + .MuiSwitch-track": {
-      backgroundColor: colour,
+      backgroundColor: secondaryColor,
     },
   }));
 
