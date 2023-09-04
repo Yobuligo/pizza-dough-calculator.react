b39707983f84be932820d4d30852d44899f59d57
Peter <hoffmannpeter82@gmail.com>
2023-09-04 12:01:55 +0200

    Introduce Footer with buttons for modalDialog

--- src/components/modalDialog/ModalDialog.tsx
+++ src/components/modalDialog/ModalDialog.tsx
@@ -1,15 +1,28 @@
 import ReactDOM from "react-dom";
+import { useTranslation } from "../../hooks/useTranslation";
 import { Card } from "../card/Card";
 import { IModalDialogProps } from "./IModalDialogProps";
 import styles from "./ModalDialog.module.css";
 
 export const ModalDialog: React.FC<IModalDialogProps> = (props) => {
+  const { t } = useTranslation();
+
   return ReactDOM.createPortal(
     <div className={styles.modalContainer}>
       <div className={styles.modalBackdrop} onClick={props.onClose}></div>
       <Card className={styles.modalDialog} style={{ width: props.width }}>
-        <h3 className={styles.modalDialogTitle}>{props.title}</h3>
-        <div>{props.component}</div>
+        <header>
+          <h3 className={styles.modalDialogHeader}>{props.title}</h3>
+        </header>
+        <main className={styles.modalDialogMain}>{props.component}</main>
+        <footer className={styles.modalDialogFooter}>
+          {props.onOkay && (
+            <button onClick={props.onOkay}>{t.dialog.okay}</button>
+          )}
+          {props.onClose && (
+            <button onClick={props.onClose}>{t.dialog.cancel}</button>
+          )}
+        </footer>
       </Card>
     </div>,
     document.getElementById("modalContainer")!
--- src/types/IModalDialogConfig.ts
+++ src/types/IModalDialogConfig.ts
@@ -3,6 +3,7 @@ import { ReactNode } from "react";
 export interface IModalDialogConfig {
   component?: ReactNode | undefined;
   onClose?: () => void;
+  onOkay?: () => void;
   show: boolean;
   title?: string;
   width?: string;
