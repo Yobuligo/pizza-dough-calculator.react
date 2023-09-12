3e6829358e4702ba3142cb1a504d11d624305073
Peter <hoffmannpeter82@gmail.com>
2023-09-12 09:45:23 +0200

    Adjust size of Icon component

--- src/components/icon/Icon.tsx
+++ src/components/icon/Icon.tsx
@@ -1,5 +1,7 @@
+import { style } from "../../utils/style";
 import { IIconProps } from "./IIconProps";
+import styles from "./Icon.module.css";
 
 export const Icon: React.FC<IIconProps> = (props) => {
-  return <span className={props.iconType}>menu</span>;
+  return <span className={style(props.iconType, styles.icon)}>menu</span>;
 };
