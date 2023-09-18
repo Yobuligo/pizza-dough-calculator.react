b2d5a9c0a2221a5b38527693a16ab9e9f9c1caa5
Peter <hoffmannpeter82@gmail.com>
2023-09-18 09:23:09 +0200

    Remove unnecessary variable

--- src/features/settings/SettingsIcon.tsx
+++ src/features/settings/SettingsIcon.tsx
@@ -1,13 +1,11 @@
 import { Icon } from "../../components/icon/Icon";
 import { useModalDialog } from "../../hooks/useModalDialog";
-import { useTranslation } from "../../hooks/useTranslation";
 import { texts } from "../../i18n/texts";
 import { IconType } from "../../types/IconType";
 import { Settings } from "./Settings";
 
 export const SettingsIcon: React.FC = () => {
   const modalDialog = useModalDialog();
-  const { t } = useTranslation();
 
   return (
     <Icon
